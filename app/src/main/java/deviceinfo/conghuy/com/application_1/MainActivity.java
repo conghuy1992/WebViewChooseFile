6cc2449a8126d34b07bfe1a33f606d4ce13f4077
huy <cong.huy@primelabo.com.vn>
2018-02-21 10:32:58 +0700

    2018-02-21

---
+++ app/src/main/java/deviceinfo/conghuy/com/application_1/MainActivity.java
@@ -0,0 +1,253 @@
+package deviceinfo.conghuy.com.application_1;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.File;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private String TAG = "MainActivity";
+    private int REQUEST_CODE = 99;
+    private String KEY = "KEY";
+    private String packageApp2 = "deviceinfo.conghuy.com.application_2";
+    private String packageApp2Class = packageApp2 + ".MainActivity";
+    private WebView mWebView;
+    private WebSettings webSettings;
+//    private String url = "http://10.0.3.2/alpine/hello.html";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webView);
+        mWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        // webSettings
+        webSettings();
+
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.setWebChromeClient(new MyWebChromeClient());
+//
+//        // load mWebView
+//        mWebView.loadUrl(url);
+        mWebView.loadUrl("file:///android_asset/hello.html");
+    }
+
+    private void webSettings() {
+        webSettings = mWebView.getSettings();
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        } else {
+            webSettings.setSavePassword(false);
+        }
+
+        webSettings.setSaveFormData(false);
+        webSettings.setJavaScriptEnabled(true);
+
+        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(true);
+
+        // enable load video
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+    }
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /**
+         * Instantiate the interface and set the context
+         */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /**
+         * get func from the web page
+         */
+        @JavascriptInterface
+        public void openApp2() {
+            launch();
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.d(TAG, "shouldOverrideUrlLoading:" + url);
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
+    }
+
+    private ValueCallback<Uri> mUploadMessage;
+    private int FILECHOOSER_RESULTCODE = 1;
+    private ValueCallback<Uri[]> mFilePathCallback;
+
+    private class MyWebChromeClient extends WebChromeClient {
+        // For Android < 3.0
+        @SuppressWarnings("unused")
+        public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+            openFileChooser(uploadMsg, "");
+        }
+
+        // For Android 3.0+
+        @SuppressWarnings("unused")
+        public void openFileChooser(ValueCallback<Uri> uploadMsg,
+                                    String acceptType) {
+            mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("image/*");
+            startActivityForResult(Intent.createChooser(i, "File Chooser"),
+                    FILECHOOSER_RESULTCODE);
+        }
+
+        // For Android 4.1+
+        @SuppressWarnings("unused")
+        public void openFileChooser(ValueCallback<Uri> uploadMsg,
+                                    String acceptType, String capture) {
+            // openFileChooser(uploadMsg, "");
+            mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("image/*");
+            startActivityForResult(Intent.createChooser(i, "File Chooser"),
+                    FILECHOOSER_RESULTCODE);
+        }
+
+        // file upload callback (Android 5.0 (API level 21) -- current) (public
+        // method)
+        @SuppressWarnings("all")
+        public boolean onShowFileChooser(WebView webView,
+                                         final ValueCallback<Uri[]> filePathCallback,
+                                         WebChromeClient.FileChooserParams fileChooserParams) {
+
+            Dexter.withActivity(MainActivity.this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .withListener(new MultiplePermissionsListener() {
+                        @Override
+                        public void onPermissionsChecked(MultiplePermissionsReport report) {
+                            if (report.areAllPermissionsGranted()) {
+                                mFilePathCallback = filePathCallback;
+                                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                                intent.setType("*/*");
+                                startActivityForResult(
+                                        Intent.createChooser(intent, "File Chooser"),
+                                        FILECHOOSER_RESULTCODE);
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Permissions are not granted!", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        }
+
+                        @Override
+                        public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                            token.continuePermissionRequest();
+                        }
+                    }).check();
+            Log.d(TAG,"onShowFileChooser");
+            return true;
+
+        }
+
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            return super.onJsAlert(view, url, message, result);
+        }
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+        }
+    }
+
+    private void launch() {
+        try {
+            Intent myIntent = new Intent();
+            myIntent.setClassName(packageApp2, packageApp2Class);
+            startActivityForResult(myIntent, REQUEST_CODE);
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + packageApp2)));
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (mUploadMessage != null) {
+                Uri result = intent == null || resultCode != Activity.RESULT_OK ? null
+                        : intent.getData();
+                if (result != null) {
+                    String path = MediaUtility.getPath(MainActivity.this,
+                            result);
+                    Uri uri = Uri.fromFile(new File(path));
+                    mUploadMessage.onReceiveValue(uri);
+                } else {
+                    mUploadMessage.onReceiveValue(null);
+                }
+            }
+        }
+        if (mFilePathCallback != null) {
+            Uri result = intent == null || resultCode != Activity.RESULT_OK ? null
+                    : intent.getData();
+            if (result != null) {
+                String path = MediaUtility.getPath(MainActivity.this, result);
+                Uri uri = Uri.fromFile(new File(path));
+                mFilePathCallback.onReceiveValue(new Uri[]{uri});
+            } else {
+                mFilePathCallback.onReceiveValue(null);
+            }
+        }
+        mUploadMessage = null;
+        mFilePathCallback = null;
+    }
+}
